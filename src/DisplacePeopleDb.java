e244ddc01a814a3c6c86c0e41af3fe2786bd60cb
IlliyaF <yiliy749@gmail.com>
2022-03-26 10:59:05 +0200

    Added Three packages

--- src/DisplacePeopleDb.java
+++ src/DisplacePeopleDb.java
@@ -1,5 +1,7 @@
 public class DisplacePeopleDb {
     public static void main(String[] args) {
         System.out.println("One package");
+        System.out.println("Two package");
+        System.out.println("Three package");
     }
 }
